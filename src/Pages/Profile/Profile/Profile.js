0dd021bb422853296f6ef17f276e67a3ecbc64ad
Nuruddin-Adnan <contact.codepixel@gmail.com>
2022-10-27 19:37:43 +0600

    firebase hosting

--- src/Pages/Profile/Profile/Profile.js
+++ src/Pages/Profile/Profile/Profile.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
 import { useContext } from 'react';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Profile = () => {
+    const [error, setError] = useState('');
     const { user, verifyEmail, notify } = useContext(AuthContext);
     const { displayName, photoURL, email, emailVerified } = user;
 
@@ -11,7 +13,7 @@ const Profile = () => {
             .then(() => {
                 notify('Email varified link send to your email.')
             })
-            .catch(error => console.error(error.message))
+            .catch(error => setError(error.message))
     }
 
     return (
@@ -20,6 +22,14 @@ const Profile = () => {
             <div className="card lg:card-side bg-base-100 shadow-xl border max-w-5xl mx-auto">
                 <figure className='overflow-hidden w-full lg:max-w-[300px]'><img className='w-full rounded' src={photoURL} alt="user avatar" /></figure>
                 <div className="card-body">
+                    {
+                        error && <div className="alert alert-error shadow-lg">
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                                <span>Error! {error}</span>
+                            </div>
+                        </div>
+                    }
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Your Email</span>
@@ -32,7 +42,7 @@ const Profile = () => {
                         </label>
                         <input type="text" defaultValue={displayName ? displayName : 'no value'} className="input input-bordered" />
                     </div>
-                    <p>Email varified: <strong>{emailVerified ? 'Varified' : `Not Varified yet`} {emailVerified || <button onClick={handleVerifyEmail} className='btn btn-sm'>Varified Now</button>}</strong></p>
+                    <p>Email varification: <strong>{emailVerified ? 'Varified' : `Not Varified yet`} {emailVerified || <button onClick={handleVerifyEmail} className='btn btn-sm'>Varified Now</button>}</strong></p>
                 </div>
             </div>
         </div>
